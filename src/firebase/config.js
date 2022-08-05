502268fff1b46fcbe782af6f5aaf6ed690574d9d
Antony Schmidt <antonyjonah@gmail.com>
2022-08-05 11:35:16 +0200

    setup firebase & define root var in index.css

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD041udhCN7MSIbOCmhJnwGxqJEy9g8Hss",
+    authDomain: "project-management-app-d0667.firebaseapp.com",
+    projectId: "project-management-app-d0667",
+    storageBucket: "project-management-app-d0667.appspot.com",
+    messagingSenderId: "989054704061",
+    appId: "1:989054704061:web:bae65c16356fd0ae375ad4"
+};
+
+//init firebase app
+const app = initializeApp(firebaseConfig)
+
+//init firestore
+const db = getFirestore(app)
+
+//init auth
+const auth = getAuth(app)
+
+export { db, auth }
